0a0ac15dc637e1bffd80d6cb5b05989da582948e
Justin Goncalves <jvgoncalves85@gmail.com>
2022-05-02 22:03:27 -0400

    Added styles to the Form

--- social-feed/src/components/DisplayPosts.jsx
+++ social-feed/src/components/DisplayPosts.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import './DisplayPosts.css'
-import Buttons from "./Buttons"
+import React from "react";
+import "./DisplayPosts.css";
+import Buttons from "./Buttons";
 const DisplayPosts = (props) => {
-    return ( 
-        <div>
-            {props.parentPosting.map((post,index)=>{
-                return(
-                    <ul>
-                        <div>
-                            <li>{post.date}</li>
-                            <li>
-                                <h3>{post.name}</h3>
-                            </li>
-                            <li>{post.post}</li>
-                            <Buttons/>
-                        </div>
-                    </ul>
-                )
-            })}
-        </div>
+  return (
+    <div>
+      {props.parentPosting.map((post, index) => {
+        return (
+          <ul className="border-box">
+            <div>
+              <li>{post.date}</li>
+              <li>
+                <h3>{post.name}</h3>
+              </li>
+              <li>{post.post}</li>
+              <Buttons />
+            </div>
+          </ul>
+        );
+      })}
+    </div>
+  );
+};
 
-    )
-}
- 
 export default DisplayPosts;
